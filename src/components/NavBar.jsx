788e5b1909db5debac14c1c87294d553537c8b19
Your Name <youremail@example.com>
2024-12-31 13:39:22 +0530

    fix: navbar resume & project archive links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Navbar({ activeSection }) {
   return (
     <>
@@ -11,7 +13,7 @@ function Navbar({ activeSection }) {
           <a
             key={id}
             href={id}
-            className={`relative pb-1 ${activeSection === id ? "font-bold" : "text-gray-500"
+            className={`relative pb-1 ${activeSection === id ? "font-bold" : "text-text"
               }`}
           >
             <span className="group relative inline-block pr-6">
@@ -43,35 +45,15 @@ function Navbar({ activeSection }) {
           { id: "all/projects", label: "Visit Project Archive" },
           { id: "https://drive.google.com/file/d/1UnHN2rxOJ0q679TGNrNLxEpOBWXbp5KU/view?usp=sharing", target: "_blank", label: "Open Resume" },
         ].map(({ id, label, target }) => (
-          <a
-            key={id}
+          <Link
+            to={id}
+            rel="noreferrer noopener"
             target={target || undefined}
-            href={id}
-            className={`relative pb-1 ${activeSection === id ? "font-bold" : "text-gray-500"
-              }`}
+            className="group relative inline-block pr-6 w-fit transition duration-300 text-text"
           >
-            <span className="group relative inline-block pr-6">
-              {/* Yellow Swipe Background */}
-              <span
-                className={`absolute inset-0 origin-left scale-x-0 transform bg-yellow-300 transition-transform duration-300 group-hover:scale-x-100 ${activeSection === id ? "scale-x-100" : ""
-                  }`}
-              ></span>
-
-              {/* Underline */}
-              <span
-                className={`absolute bottom-0 left-0 h-[2px] w-full origin-left transform bg-heading transition-all duration-300 ${activeSection === id ? "scale-x-100" : "scale-x-0"
-                  }`}
-              ></span>
-
-              {/* Text */}
-              <span
-                className={`relative z-10 transition ${activeSection === id ? "font-bold text-heading" : ""
-                  }`}
-              >
-                {label}
-              </span>
-            </span>
-          </a>
+            {label}
+            <span className="absolute inset-0 -z-10 w-0 h-full bg-yellow-300 transition-all duration-300 group-hover:w-full"></span>
+          </Link>
         ))}
       </div>
     </>
