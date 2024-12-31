8ea6d3323768cf351ebf7db875f003474f3a9ef9
Your Name <youremail@example.com>
2024-12-31 13:43:34 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/all/*" element={<Table />} />
+        <Route path="/all/projects" element={<Table />} />
       </Routes>
     </Router>
   );
